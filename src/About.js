d1b31fd84b73257bf87bac84eb058ac309812808
Petr Vorlíček <petrvorlicek7@gmail.com>
2024-06-16 10:49:28 +0200

    zacatek

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h2>O mně</h2>
+      <p>Jsem webový vývojář a rád se učím nové technologie.</p>
+    </div>
+  );
+}
+
+export default About;
